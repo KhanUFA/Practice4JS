e239cccb59620b7c0da569f928d01b3c5a5826ec
R. Starikov <lymon700@gmail.com>
2022-08-25 18:41:20 +0500

    get back

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 "use strict";
-async function load(name) {
-	const responce = await fetch("/small.csv");
 
-	if (!responce.ok) {
-		alert(responce.status);
-		return;
-	}
+let placesDB = [];
+function load(name) {
+	fetch("/small.csv")
+		.then((responce) => {
+			if (!responce.ok) {
+				throw new Error("Something get wrong! " + responce.status);
+			}
+			return responce.text();
+		})
+		.then((placesText) => {
+			buildArrayOfPlacesByReg(placesText);
+		})
+		.catch((err) => console.log(new Error(err)));
 
 	//!
 	let temp = document.createElement("div");
-	temp.innerText = await responce.text();
+	temp.innerText = textPlaces;
 	document.body.appendChild(temp);
 }
 
@@ -20,8 +27,16 @@ window.onload = () => {
 //TODO: make array considering object "Place" and add new option Lvl
 //? Use regEx.exec(). This method found 1st math and stop,
 //? and next call will show next match in String. Otherwise return null
-//*Example: "01";"512";"000";"101";"8";"2";"с Залесово";;;"493";"3";12.08.2021;01.01.2022
-function buildArrayOfPlaces() {
-	let regEx =
+//*Example line of string: "01";"512";"000";"101";"8";"2";"с Залесово";;;"493";"3";12.08.2021;01.01.2022
+function buildArrayOfPlacesByReg(placesText) {
+	const regEx =
 		/(\d+)";"(\d+)";"(\d+)";"(\d+)".+?;"([а-я]*)\s?([А-Яа-я].*?)";/g;
+	const placesArray = placesText.split("\n");
+	Console.log(placesArray[0]);
+
+	for (const iterator of placesList) {
+		const placeArray = iterator.split(regEx);
+		Console.log(placeArray[6]);
+		placesDB.push({ name: placeArray[6] });
+	}
 }
