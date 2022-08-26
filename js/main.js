6d159ac7f4f74c9f617189210bf68da94441ec36
R. Starikov <lymon700@gmail.com>
2022-08-26 18:05:46 +0500

    added methods for split text and push to "DB"

--- js/main.js
+++ js/main.js
@@ -1,42 +1,66 @@
 "use strict";
 
 let placesDB = [];
-function load(name) {
-	fetch("/small.csv")
-		.then((responce) => {
-			if (!responce.ok) {
-				throw new Error("Something get wrong! " + responce.status);
-			}
-			return responce.text();
-		})
-		.then((placesText) => {
-			buildArrayOfPlacesByReg(placesText);
-		})
-		.catch((err) => console.log(new Error(err)));
+let placesTextResponce;
+async function load(name) {
+	try {
+		const responce = await fetch(name, {
+			headers: {
+				"Content-Type": "text/plain",
+			},
+		});
 
-	//!
-	let temp = document.createElement("div");
-	temp.innerText = textPlaces;
-	document.body.appendChild(temp);
-}
-
-window.onload = () => {
-	load();
-};
-
-//TODO: make array considering object "Place" and add new option Lvl
-//? Use regEx.exec(). This method found 1st math and stop,
-//? and next call will show next match in String. Otherwise return null
-//*Example line of string: "01";"512";"000";"101";"8";"2";"с Залесово";;;"493";"3";12.08.2021;01.01.2022
-function buildArrayOfPlacesByReg(placesText) {
-	const regEx =
-		/(\d+)";"(\d+)";"(\d+)";"(\d+)".+?;"([а-я]*)\s?([А-Яа-я].*?)";/g;
-	const placesArray = placesText.split("\n");
-	Console.log(placesArray[0]);
-
-	for (const iterator of placesList) {
-		const placeArray = iterator.split(regEx);
-		Console.log(placeArray[6]);
-		placesDB.push({ name: placeArray[6] });
+		return await responce.text();
+	} catch (err) {
+		alert("Failed to open file!");
+		console.log(err);
+		return null;
 	}
 }
+
+//*Example line of string: "01";"512";"000";"101";"8";"2";"с Залесово";;;"493";"3";12.08.2021;01.01.2022
+function buildArrayOfPlacesByReg(placesText) {
+	const regEx = /(\d+)";"(\d+)";"(\d+)";"(\d+)".+?;"([А-Яа-я].*?)";/g;
+
+	let placeSplited = regEx.exec(placesText);
+	console.log(placeSplited);
+	do {
+		placesDB.push({
+			code1: Number(placeSplited[1]),
+			code2: Number(placeSplited[2]),
+			code3: Number(placeSplited[3]),
+			code4: Number(placeSplited[4]),
+			name: placeSplited[5],
+		});
+		placeSplited = regEx.exec(placeSplited.input);
+	} while (placeSplited !== null);
+	console.log("placesDB length:" + placesDB.length);
+}
+
+function buildArrayOfPlacesBySplit(placesText) {
+	const placesArray = placesText.split("\n");
+
+	for (const iterator of placesArray) {
+		const placeSplited = iterator
+			.slice(1, iterator.lastIndexOf(";;;"))
+			.split(/";"/);
+
+		placesDB.push({
+			code1: Number(placeSplited[0]),
+			code2: Number(placeSplited[1]),
+			code3: Number(placeSplited[2]),
+			code4: Number(placeSplited[3]),
+			name: placeSplited[6],
+		});
+	}
+	console.log("placesDB length:" + placesDB.length);
+}
+
+window.onload = async () => {
+	const nameFile = "/small.csv";
+	placesTextResponce = await load(nameFile);
+
+	let timeBegin = Date.now();
+	buildArrayOfPlacesByReg(placesTextResponce);
+	console.log(Date.now() - timeBegin);
+};
