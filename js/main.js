9087a1336a7e03b437703de2206208e428fa9ce7
R. Starikov <lymon700@gmail.com>
2022-08-29 16:32:28 +0500

    Cool

--- js/main.js
+++ js/main.js
@@ -18,18 +18,33 @@ async function load(name) {
 	}
 }
 
+function getPlaceLvl(code2, code3, code4) {
+	let lvl = 0;
+	if (code4 !== 0) {
+		return 0;
+	} else if (code3 !== 0) {
+		return 1;
+	} else if (code2 !== 0) {
+		return 2;
+	}
+}
+
 //*Example line of string: "01";"512";"000";"101";"8";"2";"с Залесово";;;"493";"3";12.08.2021;01.01.2022
 function buildArrayOfPlacesByReg(placesText) {
 	const regEx = /(\d+)";"(\d+)";"(\d+)";"(\d+)".+?;"([А-Яа-я].*?)";/g;
 
 	let placeSplited = regEx.exec(placesText);
-	console.log(placeSplited);
 	do {
+		const code2 = Number(placeSplited[2]);
+		const code3 = Number(placeSplited[3]);
+		const code4 = Number(placeSplited[4]);
+		const lvl = getPlaceLvl(code2, code3, code4);
 		placesDB.push({
+			lvl: lvl,
 			code1: Number(placeSplited[1]),
-			code2: Number(placeSplited[2]),
-			code3: Number(placeSplited[3]),
-			code4: Number(placeSplited[4]),
+			code2: code2,
+			code3: code3,
+			code4: code4,
 			name: placeSplited[5],
 		});
 		placeSplited = regEx.exec(placeSplited.input);
@@ -45,11 +60,16 @@ function buildArrayOfPlacesBySplit(placesText) {
 			.slice(1, iterator.lastIndexOf(";;;"))
 			.split(/";"/);
 
+		const code2 = Number(placeSplited[2]);
+		const code3 = Number(placeSplited[3]);
+		const code4 = Number(placeSplited[4]);
+		const lvl = getPlaceLvl(code2, code3, code4);
 		placesDB.push({
+			lvl: lvl,
 			code1: Number(placeSplited[0]),
-			code2: Number(placeSplited[1]),
-			code3: Number(placeSplited[2]),
-			code4: Number(placeSplited[3]),
+			code2: code2,
+			code3: code3,
+			code4: code4,
 			name: placeSplited[6],
 		});
 	}
